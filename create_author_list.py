93c8e50d632eb5b912974e0c8855865c27f9d584
Igor Andreoni <igor.andreoni@gmail.com>
2025-08-13 17:45:35 -0400

    updated comments

--- create_author_list.py
+++ create_author_list.py
@@ -2,11 +2,13 @@
 import os
 import sys
 
+# EDIT! Create a dictionary where each author name is the key to which is assigned a list of institutions
 authors = {'Igor Andreoni': ['Joint Space-Science Institute, University of Maryland, College Park, MD 20742, USA', 'Department of Astronomy, University of Maryland, College Park, MD 20742, USA', 'Astrophysics Science Division, NASA Goddard Space Flight Center, Mail Code 661, Greenbelt, MD 20771, USA'],
            'Michael W. Coughlin': ['School of Physics and Astronomy, University of Minnesota, Minneapolis, Minnesota 55455, USA'],
            'S. Bradley Cenko': ['Joint Space-Science Institute, University of Maryland, College Park, MD 20742, USA', 'Astrophysics Science Division, NASA Goddard Space Flight Center, Mail Code 661, Greenbelt, MD 20771, USA'],
     }
 
+# Create an ordered list of institutions
 institution_list_ordered = []
 for key in authors.keys():
     instutions = authors[key]
@@ -14,6 +16,7 @@ for key in authors.keys():
         if instution in institution_list_ordered: continue
         institution_list_ordered.append(instution)
 
+# Create the author list
 author_list = []
 author_list_arxiv = []
 for key in authors.keys():
@@ -24,25 +27,24 @@ for key in authors.keys():
     author_list.append('%s$^{%s}$' % (key, ",".join([str(x+1) for x in indices])))
     author_list_arxiv.append(key)
 
+# Print the author list for ArXiv
 print("List of authors for ArXiv submission:")
 print("")
 print(", ".join(author_list_arxiv))
 print("--------\n")
 
+# Print the author list for the paper
 print("List of authors for the paper:")
 print("")
 print(", ".join(author_list))
 print("")
-# Print the institutions
+
+# Print the list of institutions
 institution_list = []
 for ii, institution in enumerate(institution_list_ordered):
     institution_list.append('$^{%s}$ %s' % (str(ii+1), institution))
 print("\n".join(institution_list)) 
 print("--------\n")
 
-#institution_list = []
-#for ii, institution in enumerate(institution_list_ordered):
-#    institution_list.append('\item %s' % (institution))
-#print("\n".join(institution_list))
-
+# Print the total number of authors and institutions
 print(f"Total of {len(authors)} authors and {len(institution_list_ordered)} institutions")
